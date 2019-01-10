d3a23d40ee0c986a345bda0bc0c8c6831ccadc8c
piyush072 <piyushranjan072@gmail.com>
2019-01-10 19:09:26 +0530

    minor change log

--- studentPortal/urls.py
+++ studentPortal/urls.py
@@ -20,8 +20,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('resetpswd/',include('password_reset.urls')),
-    path('studentPortal/',include('studentPort.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('',include('django.contrib.auth.urls')),
+    path('user/', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
